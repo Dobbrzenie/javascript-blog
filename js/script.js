cb20a073bd28b4cdddbbb62bfd8ae81dc5ff4476
Michał Dobrzeniecki <dobbrzenie@gmail.com>
2023-04-08 18:50:26 +0200

    Add display article after click, change code in script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
   }); */
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
   
@@ -16,21 +17,31 @@
       activeLink.classList.remove('active');
     }
   
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE add class 'active' to the clicked link */
   
     console.log('clickedElement:', clickedElement);
     console.log('clickedElement (with plus): ' + clickedElement);
       clickedElement.classList.add('active');
   
     /* [DONE] remove class 'active' from all articles */
-  
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
-  }
-  
+      const activeArticles = document.querySelectorAll('.posts article.active');
+
+      for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+      }
+
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+
+  }    
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
