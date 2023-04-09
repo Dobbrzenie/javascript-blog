b0a170e113f75d12e21d77fa4045f4d0ad350312
Michał Dobrzeniecki <dobbrzenie@gmail.com>
2023-04-09 10:38:13 +0200

    Add generate list of titles in script.js, change in html code - delete li in links

--- js/script.js
+++ js/script.js
@@ -42,8 +42,53 @@
     targetArticle.classList.add('active');
 
   }    
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+  function generateTitleLinks(){
+
+  /*[DONE] remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = ' ';
+
+    /* find all the articles and save them to variable: articles */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for(let article of articles){
+
+     /*[DONE] get the article id */
+
+      const articleId = article.getAttribute('id');
+
+      /*[DONE] find the title element */ /*[DONE] get the title from the title element */
+
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* [DONE] create HTML of the link */
+
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+
+      /* [DONE] insert link into html variable */
+
+      html = html + linkHTML;
+      console.log(html);
+    }
+
+      titleList.innerHTML = html;
+
+      const links = document.querySelectorAll('.titles a');
+      console.log(links);
+        for(let link of links){
+        link.addEventListener('click', titleClickHandler);
+      }
+    }
+
+
+generateTitleLinks();
